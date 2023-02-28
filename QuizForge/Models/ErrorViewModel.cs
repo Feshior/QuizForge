9da06eac35fcc41ba30c0373602178a398f90f8d
Vladyslav <vladislavshvetsua@gmail.com>
2023-02-28 13:36:23 +0100

    Add project files.

---
+++ QuizForge/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace QuizForge.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
