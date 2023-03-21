93cb6144e2a6573665c06fc45512e2238a7c0ae8
Vladyslav <vladislavshvetsua@gmail.com>
2023-03-21 18:15:55 +0100

    QuizEditor development

---
+++ QuizForge/Controllers/QuestionsEditorController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using QuizForge.Data;
+using QuizForge.ViewModels.QuestionsEditor;
+
+namespace QuizForge.Controllers
+{
+    public class QuestionsEditorController : Controller
+    {
+        ApplicationDbContext dbContext;
+        public QuestionsEditorController(ApplicationDbContext context)
+        {
+            this.dbContext = context;
+        }
+        public IActionResult Index()
+        {
+            return View(
+                new QuestionsEditorIndexViewModel()
+                {
+                    QuizQuestions = dbContext.QuizQuestions.Include(q => q.QuizAnswers).ToList()
+                }
+                );
+        }
+    }
+}
---
+++ QuizForge/ViewModels/QuestionsEditor/QuestionsEditorIndexViewModel.cs
@@ -0,0 +1,8 @@
+﻿using QuizForge.Models.QuizModels;
+
+namespace QuizForge.ViewModels.QuestionsEditor
+{
+    public class QuestionsEditorIndexViewModel { 
+        public List<QuizQuestion> QuizQuestions { get; set; } = new List<QuizQuestion>();
+    }
+}
